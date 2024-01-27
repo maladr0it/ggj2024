cb12a1a5cf4208448d0c26edfdeddcd8dd93cd05
maladr0it <bradford.lachlan@gmail.com>
2024-01-27 08:40:07 -0800

    add basic movement and graphics

---
+++ src/inputSource.ts
@@ -0,0 +1,63 @@
+type InputButton =
+  | "quit"
+  | "up"
+  | "down"
+  | "left"
+  | "right"
+  | "action_a"
+  | "action_b";
+
+type InputSource = {
+  activeButtons: Set<InputButton>;
+  prevActiveButtons: Set<InputButton>;
+};
+
+const keyboardMap = {
+  escape: "quit",
+  ArrowUp: "up",
+  ArrowDown: "down",
+  ArrowLeft: "left",
+  ArrowRight: "right",
+  z: "action_a",
+  x: "action_b",
+} as const;
+
+const isInputKey = (key: string): key is keyof typeof keyboardMap => {
+  return key in keyboardMap;
+};
+
+export const inputSource_create = (): InputSource => {
+  const activeButtons = new Set<InputButton>();
+  const prevActiveButtons = new Set<InputButton>();
+
+  return { activeButtons, prevActiveButtons };
+};
+
+export const inputSource_read = (source: InputSource) => {
+  const pressedButtons = new Set<InputButton>();
+  for (const key of source.activeButtons.values()) {
+    if (!source.prevActiveButtons.has(key)) {
+      pressedButtons.add(key);
+    }
+  }
+
+  // prepare the input state for the next time it is read
+  source.prevActiveButtons = new Set(source.activeButtons);
+
+  return {
+    activeButtons: source.activeButtons,
+    pressedButtons,
+  };
+};
+
+export const inputSource_handleKeyDown = (source: InputSource, key: string) => {
+  if (isInputKey(key)) {
+    source.activeButtons.add(keyboardMap[key]);
+  }
+};
+
+export const inputSource_handleKeyUp = (source: InputSource, key: string) => {
+  if (isInputKey(key)) {
+    source.activeButtons.delete(keyboardMap[key]);
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,118 @@
+import * as PIXI from "pixi.js";
+
+import {
+  inputSource_create,
+  inputSource_handleKeyDown,
+  inputSource_handleKeyUp,
+  inputSource_read,
+} from "./inputSource";
+
+import { entity_create, entity_render } from "./entity";
+
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = ``;
+const GRAVITY = 0.1;
+const GROUND_LEVEL = 100;
+const JUMP_VEL = -3;
+
+const app = new PIXI.Application({
+  width: 600,
+  height: 150,
+  background: "red",
+});
+
+//
+// Load assets
+//
+const runSprite = PIXI.AnimatedSprite.fromImages([
+  "sprites/dino-run1.png",
+  "sprites/dino-run2.png",
+]);
+runSprite.anchor.set(0, 1); // anchor to bottom so we can test for ground contact more easily
+runSprite.animationSpeed = 0.1; // what does 0.1 mean exactly?
+
+const jumpSprite = PIXI.AnimatedSprite.fromImages(["sprites/dino-jump1.png"]);
+jumpSprite.anchor.set(0, 1);
+jumpSprite.animationSpeed = 0.1;
+
+const groundSprite1 = PIXI.AnimatedSprite.fromImages(["sprites/ground.png"]);
+groundSprite1.anchor.set(0, 1);
+const groundSprite2 = PIXI.AnimatedSprite.fromImages(["sprites/ground.png"]);
+groundSprite2.anchor.set(0, 1);
+
+//
+// game state
+//
+const keyboard = inputSource_create();
+
+let runSpeed = 3;
+
+const dino = entity_create(runSprite);
+const ground1 = entity_create(groundSprite1);
+const ground2 = entity_create(groundSprite2);
+
+//
+// Main loop
+//
+const tick = (dt: number) => {
+  const { activeButtons, pressedButtons } = inputSource_read(keyboard);
+
+  // dino is in the air
+  if (dino.y < GROUND_LEVEL) {
+    dino.dy += GRAVITY * dt;
+  }
+
+  dino.y = Math.min(dino.y + dino.dy * dt, GROUND_LEVEL);
+
+  // dino landed on the ground
+  if (dino.y === GROUND_LEVEL) {
+    dino.sprite = runSprite;
+  }
+
+  // dino jumped
+  if (pressedButtons.has("up") && dino.y === GROUND_LEVEL) {
+    dino.dy = JUMP_VEL;
+    dino.sprite = jumpSprite;
+  }
+
+  // move the ground
+  // TODO: make the ground cycle forever
+  ground1.x -= runSpeed;
+  ground2.x -= runSpeed;
+
+  // render (update sprites)
+  //
+  entity_render(ground1, app.stage);
+  entity_render(ground2, app.stage);
+  entity_render(dino, app.stage);
+};
+
+const startButtonClick = () => {
+  dino.x = 50;
+  dino.y = GROUND_LEVEL;
+
+  ground1.y = GROUND_LEVEL;
+  ground1.x = 0;
+  ground2.y = GROUND_LEVEL;
+  ground2.x = ground1.sprite.width;
+
+  app.ticker.add(tick);
+};
+
+//
+// Add stuff to DOM
+//
+// @ts-ignore
+document.body.appendChild(app.view);
+
+document.addEventListener("keydown", (event) => {
+  inputSource_handleKeyDown(keyboard, event.key);
+});
+document.addEventListener("keyup", (event) => {
+  inputSource_handleKeyUp(keyboard, event.key);
+});
+
+const startButton = document.createElement("button");
+startButton.innerText = "Start";
+startButton.addEventListener("click", startButtonClick);
+document.body.appendChild(startButton);
