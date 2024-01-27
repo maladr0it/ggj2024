1c9be9983b08f29a683358c9e0139fb122bd2873
maladr0it <bradford.lachlan@gmail.com>
2024-01-27 11:47:06 -0800

    remove entity

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
   inputSource_handleKeyUp,
   inputSource_read,
 } from "./inputSource";
-import { entity_create, entity_render } from "./entity";
+// import { entity_create, entity_render } from "./entity";
 import { log_clear, log_getContent, log_write } from "./log";
 
 import "./style.css";
@@ -27,91 +27,74 @@ const app = new PIXI.Application({
 //
 // Load assets
 //
-const runSprite = PIXI.AnimatedSprite.fromImages([
-  "sprites/dino-run1.png",
-  "sprites/dino-run2.png",
-]);
-runSprite.anchor.set(0, 1); // anchor to bottom so we can test for ground contact more easily
-runSprite.animationSpeed = 0.1; // what does 0.1 mean exactly?
-
-const jumpSprite = PIXI.AnimatedSprite.fromImages(["sprites/dino-jump1.png"]);
-jumpSprite.anchor.set(0, 1);
-jumpSprite.animationSpeed = 0.1;
-
-const groundSprite1 = PIXI.AnimatedSprite.fromImages(["sprites/ground.png"]);
-groundSprite1.anchor.set(0, 1);
-const groundSprite2 = PIXI.AnimatedSprite.fromImages(["sprites/ground.png"]);
-groundSprite2.anchor.set(0, 1);
-
-const cactusSprite1 = PIXI.AnimatedSprite.fromImages(["sprites/cactus1.png"]);
-cactusSprite1.anchor.set(0, 1);
+const runAnim = [
+  PIXI.Texture.from("sprites/dino-run1.png"),
+  PIXI.Texture.from("sprites/dino-run2.png"),
+];
+const jumpAnim = [PIXI.Texture.from("sprites/dino-jump1.png")];
 
 //
 // game state
 //
 const keyboard = inputSource_create();
 
+const dino = {
+  state: "",
+  sprite: new PIXI.AnimatedSprite(runAnim),
+  dy: 0,
+  dx: 0,
+};
+dino.sprite.anchor.set(0, 1);
+dino.sprite.animationSpeed = 0.1;
+
 let distance = 0; // distance the dino has travelled
 let runSpeed = 10;
-
-const dino = entity_create(runSprite);
-let ground1 = entity_create(groundSprite1);
-let ground2 = entity_create(groundSprite2);
 
 //
 // Main loop
 //
 const tick = (dt: number) => {
+  log_write("isplaying", dino.sprite.playing);
+
   const { activeButtons, pressedButtons } = inputSource_read(keyboard);
 
   // Apply forces
+  const prevDinoY = dino.sprite.y;
   dino.dy += GRAVITY * dt;
-  dino.y = Math.min(dino.y + dino.dy * dt, GROUND_LEVEL);
-
-  // dino landed on the ground
-  if (dino.y === GROUND_LEVEL) {
-    dino.sprite = runSprite;
+  dino.sprite.y = Math.min(dino.sprite.y + dino.dy * dt, GROUND_LEVEL);
+  // dino hit the ground
+  if (prevDinoY < GROUND_LEVEL && dino.sprite.y === GROUND_LEVEL) {
+    dino.sprite.textures = runAnim;
+    dino.sprite.play();
   }
 
   // dino jumped
-  if (pressedButtons.has("up") && dino.y === GROUND_LEVEL) {
+  if (pressedButtons.has("up") && dino.sprite.y === GROUND_LEVEL) {
     dino.dy = JUMP_VEL;
-    dino.sprite = jumpSprite;
+    dino.sprite.textures = jumpAnim;
+    dino.sprite.play();
   }
 
   // move the ground
   distance += runSpeed;
-  ground1.x -= runSpeed;
-  ground2.x -= runSpeed;
-
-  // if the ground has gone offscreen, move it
-  if (ground1.x + ground1.sprite.width < 0) {
-    ground1.x = ground2.x + ground2.sprite.width;
-    const temp = ground1;
-    ground1 = ground2;
-    ground2 = temp;
-  }
 
   log_write("distance:", distance);
-
-  // render (update sprites)
-  //
-  entity_render(ground1, app.stage);
-  entity_render(ground2, app.stage);
-  entity_render(dino, app.stage);
 
   logEl.innerText = log_getContent();
   log_clear();
 };
 
 const startButtonClick = () => {
-  dino.x = 50;
-  dino.y = GROUND_LEVEL;
+  app.stage.addChild(dino.sprite);
+  dino.sprite.textures = runAnim;
+  dino.sprite.x = 50;
+  dino.sprite.y = GROUND_LEVEL;
+  dino.sprite.play();
 
-  ground1.y = GROUND_LEVEL;
-  ground1.x = 0;
-  ground2.y = GROUND_LEVEL;
-  ground2.x = ground1.sprite.width;
+  // ground1.y = GROUND_LEVEL;
+  // ground1.x = 0;
+  // ground2.y = GROUND_LEVEL;
+  // ground2.x = ground1.sprite.width;
 
   app.ticker.add(tick);
 };
