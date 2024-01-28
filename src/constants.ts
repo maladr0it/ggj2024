3823997b5db9d23db0c739d78c794e2c2a07bcf9
maladr0it <bradford.lachlan@gmail.com>
2024-01-28 10:54:13 -0800

    add debug flag

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,5 @@ export const JUMP_VEL = -700;
 export const DEATH_DECEL = 750;
 export const RUN_SPEED = 360;
 export const SCORE_MULTIPLIER = 0.005; // Modifies rate score increases relative to distance
+
+export const DEBUG = false;
