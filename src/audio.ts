5b5610b0f88c7db672ec3895b64c377bfbc04e56
benwest <ben.s.west@gmail.com>
2024-01-28 10:45:15 -0800

    audio bits

--- src/audio.ts
+++ src/audio.ts
@@ -1,10 +1,9 @@
 import { Player } from "tone";
 import { Assets, assets } from "./assets";
-import { lerp } from "./utils/math";
 
 type AudioName = keyof Assets["audio"];
-export const playSound = (name: AudioName, varyPitch = false) => {
+export const playSound = (name: AudioName) => {
   const player = new Player(assets().audio[name]);
-  if (varyPitch) player.playbackRate = lerp(0.8, 1.2, Math.random());
   player.toDestination().start();
+  return player;
 };
